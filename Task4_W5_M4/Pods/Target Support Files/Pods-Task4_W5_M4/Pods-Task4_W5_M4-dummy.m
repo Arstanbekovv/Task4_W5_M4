75601f067b4d7f4174bd8556ab2a284e90af515a
Arstanbekovv <94747115+Arstanbekovv@users.noreply.github.com>
2022-01-18 19:05:09 +0600

    пгпгп

---
+++ Task4_W5_M4/Pods/Target Support Files/Pods-Task4_W5_M4/Pods-Task4_W5_M4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Task4_W5_M4 : NSObject
+@end
+@implementation PodsDummy_Pods_Task4_W5_M4
+@end
---
+++ Task4_W5_M4/Task4_W5_M4/File1.swift
@@ -0,0 +1,20 @@
+//
+//  File1.swift
+//  Task4_W5_M4
+//
+//  Created by Aziz on 16/1/22.
+//
+
+import Foundation
+
+class NewsModel {
+    var title: String
+    var description: String
+    var image: String
+    
+    init(title: String, description: String, image: String) {
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+}
